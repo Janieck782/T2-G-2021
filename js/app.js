4b76f4c9a5100977c3814394e778e5115db53d86
Kalcio <matialem22@gmail.com>
2021-06-09 22:03:06 -0400

    V.1.2.2 - Opción alfabeto números y letras añadida

--- js/app.js
+++ js/app.js
@@ -28,8 +28,8 @@ function iniciarAutomata(automatas){//Funcion para iniciar automatas
     console.log(automatas);
 }
 
-function estados(automatas,in){
-    const aux = document.getElementById(in).value;
+function estados(automatas, di){
+    const aux = document.getElementById(di).value;
     console.log(aux);
 
 }
@@ -40,24 +40,42 @@ function tamañoAlfabeto(){
 }
 
 function asignarAlfabeto(automatas){
-    let aux = tamañoAlfa;
+    let aux = tamañoAlfa, i;
+    var tipo = tipo_alfa(), cont = 0;
+
     automatas.s = []
-    for(let i = 0 ; i<aux ; i++){
-        automatas.s.push(String.fromCharCode(97+i))
-    }
+
+    if(tipo == false) {
+        for(i = 0; i < aux; i++) {
+            automatas.s.push(String.fromCharCode(97+i))
+        }
+    } else {
+        for(i = 0; i < aux; i++) {
+            automatas.s.push(cont);
+            cont++;
+        }
+    }    
 }
 
 function imprimirAlfabeto(){
     let aux = tamañoAlfabeto();
-    tamañoAlfa = aux; 
+    tamañoAlfa = aux;
+    var tipo = tipo_alfa(), i;
     let letra = String.fromCharCode(97)
-    document.getElementById("alfabetoDescripcion").innerHTML += ` El abecedario es:`;
+    document.getElementById("alfabetoDescripcion").innerHTML += ` El alfabeto es:`;
 
-    for(let i = 0 ; i<aux ; i++){
-        letra = String.fromCharCode(97+i);
-        document.getElementById("alfabetoDescripcion").innerHTML += ` ${letra},`;
+    if(tipo == false) {
+        for(i = 0; i<aux; i++){
+            letra = String.fromCharCode(97+i);
+            document.getElementById("alfabetoDescripcion").innerHTML += ` ${letra},`;
+        }
+    } else {
+        var cont = 0;
+        for(i = 0; i < aux; i++) {
+            document.getElementById("alfabetoDescripcion").innerHTML += ` ${cont},`;
+            cont++;
+        }
     }
-    
 }
 
 function afd(){ //Funcion que define AFD o AFND
@@ -66,6 +84,11 @@ function afd(){ //Funcion que define AFD o AFND
         return true;//AFD      
     }
     else return false;//AFND
+}
+
+function tipo_alfa() {
+    var aux = document.getElementById("alfab").value;
+    return aux == 0; //letra
 }
 
 
