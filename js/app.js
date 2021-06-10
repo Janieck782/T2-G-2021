2a8bb56ff5d1bbe2d5c8ad8f19ce94a8ef2d3e3f
Kalcio <matialem22@gmail.com>
2021-06-09 22:09:24 -0400

    Arreglo del código

--- js/app.js
+++ js/app.js
@@ -6,10 +6,10 @@
 
 //Declaracion de automata
 const automata = {
-    k : [], //estados = q0, q1, q2....
-    s : [], //alfabeto = a, b....
-    g : [], //caminos = ((q0,a)q1), ((q1,b)q0)...
-    f : [], //final = q0, q1, q2....
+    k: [], //estados = q0, q1, q2....
+    s: [], //alfabeto = a, b....
+    g: [], //caminos = ((q0,a)q1), ((q1,b)q0)...
+    f: [], //final = q0, q1, q2....
     afd: null // afnd o afd
 }
 
@@ -22,68 +22,70 @@ const automata2 = Object.assign(automata);
 
 
 
-function iniciarAutomata(automatas){//Funcion para iniciar automatas
+function iniciarAutomata(automatas) { //Funcion para iniciar automatas
     automatas.afd = afd();
     asignarAlfabeto(automatas);
     console.log(automatas);
 }
 
-function estados(automatas, di){
+function estados(automatas, di) {
     const aux = document.getElementById(di).value;
     console.log(aux);
 
 }
 
-function tamañoAlfabeto(){
+function tamañoAlfabeto() {
     const aux = document.getElementById("alfabeto").value;
     return aux;
 }
 
-function asignarAlfabeto(automatas){
-    let aux = tamañoAlfa, i;
-    var tipo = tipo_alfa(), cont = 0;
+function asignarAlfabeto(automatas) {
+    let aux = tamañoAlfa,
+        i;
+    var tipo = tipo_alfa(),
+        cont = 0;
 
     automatas.s = []
 
-    if(tipo == false) {
-        for(i = 0; i < aux; i++) {
-            automatas.s.push(String.fromCharCode(97+i))
+    if (tipo == false) {
+        for (i = 0; i < aux; i++) {
+            automatas.s.push(String.fromCharCode(97 + i))
         }
     } else {
-        for(i = 0; i < aux; i++) {
+        for (i = 0; i < aux; i++) {
             automatas.s.push(cont);
             cont++;
         }
-    }    
+    }
 }
 
-function imprimirAlfabeto(){
+function imprimirAlfabeto() {
     let aux = tamañoAlfabeto();
     tamañoAlfa = aux;
-    var tipo = tipo_alfa(), i;
+    var tipo = tipo_alfa(),
+        i;
     let letra = String.fromCharCode(97)
     document.getElementById("alfabetoDescripcion").innerHTML += ` El alfabeto es:`;
 
-    if(tipo == false) {
-        for(i = 0; i<aux; i++){
-            letra = String.fromCharCode(97+i);
+    if (tipo == false) {
+        for (i = 0; i < aux; i++) {
+            letra = String.fromCharCode(97 + i);
             document.getElementById("alfabetoDescripcion").innerHTML += ` ${letra},`;
         }
     } else {
         var cont = 0;
-        for(i = 0; i < aux; i++) {
+        for (i = 0; i < aux; i++) {
             document.getElementById("alfabetoDescripcion").innerHTML += ` ${cont},`;
             cont++;
         }
     }
 }
 
-function afd(){ //Funcion que define AFD o AFND
+function afd() { //Funcion que define AFD o AFND
     const aux = document.getElementById("AFD").value;
-    if(aux == 0){
-        return true;//AFD      
-    }
-    else return false;//AFND
+    if (aux == 0) {
+        return true; //AFD      
+    } else return false; //AFND
 }
 
 function tipo_alfa() {
@@ -95,9 +97,8 @@ function tipo_alfa() {
 
 //funciones html
 
-function desactiva_enlace(enlace)
-{
-      enlace.disabled='disabled';
+function desactiva_enlace(enlace) {
+    enlace.disabled = 'disabled';
 }
 
 window.addEventListener("load", () => {
